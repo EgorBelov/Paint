a49d1f6c2aaa85848f4b0e3c272f4e6574f29020
Егор Белов <e1belov@yandex.ru>
2023-01-28 16:43:36 +0500

    Add project files.

---
+++ MainForm.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Paint
+{
+    public partial class MainForm : Form
+    {
+        public static Color Color { get; set; }
+        public static int Width { get; set; }
+
+
+        public MainForm()
+        {
+            InitializeComponent();
+            Color = Color.Black;
+            Width = 3;
+        }
+
+        private void MaimForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+        private void новыйToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var frm = new DocumentForm();
+            frm.MdiParent = this;
+            frm.Show();
+        }
+
+        private void toolStripLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void оПрограммеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var frmAbout = new AboutForm();
+            frmAbout.ShowDialog();
+        }
+    }
+}
